325caa65742e96b5f2247784f4f341f43f164936
Stefan Golban <escgamingt.o@gmail.com>
2021-09-22 17:02:31 +0300

    fix: fixing production module importing error

---
+++ tsconfig-paths-bootstrap.js
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+// tsconfig-paths-bootstrap.js
+
+const tsConfig = require('./tsconfig.json')
+const tsConfigPaths = require('tsconfig-paths')
+
+const baseUrl = './dist' // Either absolute or relative path. If relative it's resolved to current working directory.
+tsConfigPaths.register({
+  baseUrl,
+  paths: tsConfig.compilerOptions.paths,
+})
