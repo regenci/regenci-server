9aad8e215d3891e8e526a7b61edf31d848098290
Stefan Golban <escgamingt.o@gmail.com>
2021-09-22 18:02:27 +0300

    feat: added ip lookup for the client ip

--- src/services/app.service.ts
+++ src/services/app.service.ts
@@ -1,8 +1,12 @@
+import geoip from 'geoip-lite'
 import { Injectable } from '@nestjs/common'
 
 @Injectable()
 export class AppService {
   root(ip: string): string {
-    return `Hello, welcome to regenci main server! Your IP is: ${ip}`
+    return `Hello, welcome to regenci main server! 
+              Some details about you:
+              - Your IP is: ${ip}
+              - ${geoip.lookup(ip)}`
   }
 }
