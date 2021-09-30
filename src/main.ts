33df40dc36ddb50a092d74d51e4d18a027a2a66f
Stefan Golban <escgamingt.o@gmail.com>
2021-09-30 20:11:51 +0300

    feat: added vercel file, paths

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,9 @@
-import { AppModule } from './components';
-import { NestFactory } from '@nestjs/core';
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify';
+import { AppModule } from '@server/components/app'
+import { NestFactory } from '@nestjs/core'
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify'
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter(),
-  );
-  await app.listen(3000);
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter())
+  await app.listen(3000)
 }
-bootstrap();
+bootstrap()
