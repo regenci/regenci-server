3da64a79162fee8f586fd939580d49871e4bb92f
stephen-golban <golban.stephen@gmail.com>
2021-12-12 18:21:30 +0200

    fixed .gitignore

--- dist/prisma/prisma.service.d.ts
+++
@@ -1,6 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-import { OnModuleInit, INestApplication } from '@nestjs/common';
-export declare class PrismaService extends PrismaClient implements OnModuleInit {
-    onModuleInit(): Promise<void>;
-    enableShutdownHooks(app: INestApplication): Promise<void>;
-}
